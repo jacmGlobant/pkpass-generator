584672194bed43e2321af69af4405ca18d55ecff
Juan Canepa <juan.canepa@globant.com>
2023-11-02 11:26:56 -0300

    feat: compress and extension change zip to pkpass functionality added

--- pkpassGenerator.js
+++ pkpassGenerator.js
@@ -5,40 +5,40 @@
  *  For sign in: openssl smime -binary -sign -certfile ${config.appleWWDRCACertificatePath} -signer ${config.passCertificatePath} -inkey ${config.passCertificateKeyPath} -in ${tempPassFolder}/manifest.json -out ${tempPassFolder}/signature -outform DER -passin pass:${config.certificatePassword}
  *  PassSigner => password: Test@123
 */
+
+const path = require('path');
 const fs = require('fs');
 const fse = require('fs-extra');
 const hasha = require('hasha');
 const { exec } = require('child_process');
 const util = require('util');
-const path = require('path');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const readdir = util.promisify(fs.readdir);
 
-const ROOT_PATH = './';
-const SAMPLE_PASS = './passes/sample';
-const OUTPUT_PASS = './output';
+const SAMPLE_PASS_DIR = './passes/sample';
+const OUTPUT_PASS_DIR = './output/pass';
 
-const DS_STORE_FILE = 'output/.DS_Store';
-const SIGNATURE_FILE = 'output/signature' 
-const MANIFEST_FILE = 'output/manifest.json';
+const DS_STORE_FILE = path.resolve('output/.DS_Store');
+const SIGNATURE_FILE = path.resolve('output/signature') 
+const MANIFEST_FILE = path.resolve('output/manifest.json');
 
 const _createTemporaryDirectory = () => {
    try {
-      if (fs.existsSync(OUTPUT_PASS)) {
-         fs.rmSync(OUTPUT_PASS, { recursive: true, force: true });
+      if (fs.existsSync(OUTPUT_PASS_DIR)) {
+         fs.rmSync(OUTPUT_PASS_DIR, { recursive: true, force: true });
       } 
    } catch (error) {
       // log system here
    } finally {
-      fs.mkdirSync(OUTPUT_PASS);
+      fs.mkdirSync(OUTPUT_PASS_DIR);
    }
 }
 
 const _copyPassToTemporaryLocation = () => {
    try {
-      fse.copySync(SAMPLE_PASS, OUTPUT_PASS, { overwrite: true });
+      fse.copySync(SAMPLE_PASS_DIR, OUTPUT_PASS_DIR, { overwrite: true });
    } catch (error) {
       // log system here
    }
@@ -71,12 +71,12 @@ const _buildPassFormat = () => {
 const _generateJSONManifest = async () => {
    try {
 
-      const files = await readdir(OUTPUT_PASS);
+      const files = await readdir(OUTPUT_PASS_DIR);
 
       const manifest = {};
 
       for (const file of files) {
-         const filePath = path.join(OUTPUT_PASS, file);
+         const filePath = path.join(OUTPUT_PASS_DIR, file);
          const fileBuffer = await readFile(filePath);
          const hash = await hasha(fileBuffer, {algorithm: 'sha1'});
          manifest[file] = hash;
@@ -84,7 +84,7 @@ const _generateJSONManifest = async () => {
 
       const jsonContent = JSON.stringify(manifest, null, 3); // Pretty print with 3 spaces indentation
 
-      const manifestFilePath = path.join(OUTPUT_PASS, 'manifest.json');
+      const manifestFilePath = path.join(OUTPUT_PASS_DIR, 'manifest.json');
       await writeFile(manifestFilePath, jsonContent);
 
       console.log('Manifest JSON created :D');
@@ -94,34 +94,27 @@ const _generateJSONManifest = async () => {
    }
 };
 
+
 const config = {
-   appleWWDRCACertificatePath: '/Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/pass2.cer',
-   passCertificatePath: '/Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/passSigner2.p12',
-   passCertificateKeyPath: '/Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/privateKey.pem',
-   manifestFilePath: '/Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/output/manifest.json',
-   signatureFilePath: '/Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/output/signature',
+   appleWWDRCACertificatePath: path.resolve('certificates/WWDRG4.pem'),
+   passCertificatePath: path.resolve('certificates/signerPass.p12'),
+   passCertificateKeyPath: path.resolve('certificates/privateKey.pem'),
+   manifestFilePath: path.resolve('output/pass/manifest.json'),
+   signatureFilePath: path.resolve('output/pass/signature'),
    certificatePassword: 'Test@123'
 };
 
 const _signJSONManifest = async () => {
    try {
-      const command = `openssl smime -binary -sign -certfile ${config.appleWWDRCACertificatePath}` +
-                  `-signer ${config.passCertificatePath}` +
-                  `-inkey ${config.passCertificateKeyPath}` +
-                  `-in ${manifestFilePath} ` +
-                  `-out ${signatureFilePath} ` + 
-                  `-outform DE` +
-                  `-passin pass:${config.certificatePassword}`;
-
-/*       const command = `openssl smime -binary -sign -certfile /Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/AppleWWDRCAG4.cer \
-   -signer /Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/PassSigner.p12 \
-   -inkey /Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/certificates/privateKey.pem \
-   -in /Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/output/manifest.json \
-   -out /Users/lissette.vasquez/Projects/PKPASS/pkpass-generator/output/signature \
-   -outform DER -passin pass:Test@123` */
+      const command = `openssl smime -binary \
+                        -sign -certfile ${config.appleWWDRCACertificatePath} \
+                        -signer ${config.passCertificatePath} \
+                        -inkey ${config.passCertificateKeyPath} \
+                        -in ${config.manifestFilePath} \
+                        -out ${config.signatureFilePath} \
+                        -outform DER -passin pass:${config.certificatePassword}`;
 
    await execPromise(command);
-
    console.log('Manifest JSON signed successfully');
    } catch (error) {
       console.log('Error signing json manifest :(', error)
@@ -129,8 +122,8 @@ const _signJSONManifest = async () => {
 }
 
 const execPromise = (command) => {
-   return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
+   return new Promise((resolve, _) => {
+      exec(command, (error, stdout, _) => {
          if (error) {
             console.log('error in promise', error);
          }
@@ -141,27 +134,25 @@ const execPromise = (command) => {
 };
 
 const _compressPassFile = async () => {
-   const CMD = 'ls';
-   exec(CMD, (err, stdout, stderr) => {
+   process.chdir('output');
+   const CMD = `zip -r pass.zip pass && mv pass.zip pass.pkpass`;
+   exec(CMD, (err, stdout, _) => {
       if (err) {
-        return;
+        console.log('Ups, something was wrong!')
       }
       console.log(stdout);
    });
-   console.log('> _signJSONManifest called');
+   console.log('> _compressPassFile called');
 } 
 
 const signPass = async () => {
    await _createTemporaryDirectory();
    await _copyPassToTemporaryLocation();
    await _cleanDSStoreFiles();
-
-   await _buildPassFormat();
-
+   // await _buildPassFormat();
    await _generateJSONManifest();
    await _signJSONManifest();
    await _compressPassFile();
-   console.log('signPass called !');
 }
 
 module.exports = signPass;
